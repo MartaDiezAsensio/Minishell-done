e347145deedf8630b257a7bd399656ad798470ed
Marta Diez Asensio <114190534+MartaDiezAsensio@users.noreply.github.com>
2023-10-26 16:33:52 +0200

    Minishell-done with mistakes solved

---
+++ src/libft/ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/20 16:21:35 by mbari             #+#    #+#             */
+/*   Updated: 2023/10/26 16:08:01 by mdiez-as         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_store(char *str, int int_size, int n)
+{
+	int	i;
+
+	if (n > 0)
+		i = 1;
+	else
+		i = 0;
+	str[int_size] = '\0';
+	while (i <= --int_size)
+	{
+		str[int_size] = n % 10 + '0';
+		n /= 10;
+		i++;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	//int				i;
+	char			*str;
+	int				int_size;
+
+	//i = 0;
+	int_size = ft_intsize(n);
+	str = (char *)malloc(sizeof(char) * int_size + 1);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (n <= 0)
+		int_size += 1;
+	if (!str)
+		return (NULL);
+	if (n <= 0)
+	{
+		//i = 1;
+		if (n == 0)
+			str[0] = '0';
+		else
+			str[0] = '-';
+		n = -n;
+	}
+	ft_store(str, int_size, n);
+	return (str);
+}
